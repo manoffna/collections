2c47649daf2dbebdbc1962f81ccc159ecfb0c774
manoffna <2780nm@gmail.com>
2023-03-13 01:36:52 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введите текст");
+            Scanner in = new Scanner(System.in);
+            String string = in.nextLine();
+            String[] words = string.split("\\s+");
+            HashMap<String, Integer> wordToCount = new HashMap<>();
+            for (String word : words)
+            {
+                if (!wordToCount.containsKey(word))
+                {
+                    wordToCount.put(word, 0);
+                }
+                wordToCount.put(word, wordToCount.get(word) + 1);
+            }
+            for (String word : wordToCount.keySet())
+            {
+                System.out.println(word + " " + wordToCount.get(word));
+            }
+        }
+}
